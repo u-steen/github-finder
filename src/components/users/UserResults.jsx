e6768b240e261c1feee9813e4fef7bc52d70fd9f
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-25 18:33:10 +0300

    displaying the first 30 gh users on the first page

---
+++ src/components/users/UserResults.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useState} from "react";
+import Spinner from "../layout/Spinner.jsx";
+import UserItem from "./UserItem.jsx";
+
+function UserResults(){
+    const [users, setUsers] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        fetchUsers()
+    }, [])
+
+    const fetchUsers = async () => {
+        setIsLoading(true)
+        const response = await fetch(`${import.meta.env.VITE_GITHUB_URL}/users`, {
+            headers: {
+                Authorization: `token ${import.meta.env.VITE_GITHUB_TOKEN}`
+            }
+        })
+
+        const data = await response.json()
+        setUsers(data)
+        setIsLoading(false)
+    }
+
+    if(!isLoading) {
+
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user}/>
+                ))}
+
+            </div>
+        )
+    } else {
+        return (
+            <Spinner/>
+        )
+    }
+
+}
+
+export default UserResults
