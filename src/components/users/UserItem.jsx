637a92e0804617323e23ad1e7b38d827c062f9a5
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-28 17:11:37 +0300

    corrected visit profile link

--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,4 +1,4 @@
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
 
 function UserItem({user: {login, avatar_url}}){
     return (
@@ -13,11 +13,11 @@ function UserItem({user: {login, avatar_url}}){
                 </div>
                 <div>
                     <h2 className="card-title">{login}</h2>
-                    <Link className="text-base-content text-opacity-40" to={`/users/${login}`}>Visit Profile</Link>
+                    <Link className="text-base-content text-opacity-40" to={`/user/${login}`}>Visit Profile</Link>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default UserItem
+export default UserItem;
