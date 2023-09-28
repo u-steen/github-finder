f82c4a8c4dd3a469237ba7ec5f95058a53ff89ea
Iustin Esanu <iustin.esanu7@gmail.com>
2023-09-28 17:12:21 +0300

    added bottom margin so the active border doesn't collapse with UserList

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -18,7 +18,7 @@ function UserSearch(){
     };
     const handleClear = () => clearUsers();
     return (
-        <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2">
+        <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-6">
             <div>
                 <form onSubmit={handleSubmit}>
                     <div className="form-control">
